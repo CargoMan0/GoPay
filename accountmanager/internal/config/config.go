1c568d26cc99209b9265bf777b7bfdd9cdd9d16c
mondy <workplacefocus@proton.me>
2025-10-08 14:47:45 +0300

    started making cfg

--- accountmanager/internal/config/config.go
+++ accountmanager/internal/config/config.go
@@ -1,30 +1,54 @@
 package config
 
+import (
+	"flag"
+	"fmt"
+	"github.com/spf13/viper"
+)
+
 type Config struct {
-	GRPCServer   GRPCServer
-	TokenManager TokenManager
-	EventSender  EvenSender
-	Producer     Producer
+	GRPCServer   GRPCServer   `mapstructure:"grpc_server"`
+	TokenManager TokenManager `mapstructure:"token_manager"`
+	EventSender  EvenSender   `mapstructure:"event_sender"`
+	Producer     Producer     `mapstructure:"producer"`
 }
 
 type Producer struct {
 }
 
 type GRPCServer struct {
-	Port int
+	Port int `mapstructure:"port"`
 }
 
 type TokenManager struct {
-	Secret string
+	Secret string `mapstructure:"secret"`
 }
 
 type EvenSender struct {
-	HandlePeriodSeconds int
-	MaxBatchSize        uint8
+	HandlePeriodSeconds uint8 `mapstructure:"handle_period_seconds"`
+	MaxBatchSize        uint8 `mapstructure:"max_batch_size"`
 }
 
-func Load() (*Config, error) {
-	var cfg Config
+func Load() (Config, error) {
+	path := getPath()
 
-	return &cfg, nil
+	var cfg Config
+	viper.SetConfigFile(path)
+
+	if err := viper.ReadInConfig(); err != nil {
+		return cfg, fmt.Errorf("error reading config file: %w", err)
+	}
+
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return cfg, fmt.Errorf("unable to unmarshal config: %w", err)
+	}
+
+	return cfg, nil
+}
+
+func getPath() string {
+	configPath := flag.String("config", "./config", "path to the config file")
+	flag.Parse()
+
+	return *configPath
 }
