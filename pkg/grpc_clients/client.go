8de7ec093d3d23d35a778ddd26fd87b617f348b7
mondy <workplacefocus@proton.me>
2025-10-09 16:27:04 +0300

    create grpc clients

--- pkg/grpc_clients/client.go
+++ pkg/grpc_clients/client.go
@@ -1,5 +1,13 @@
 package grpc_clients
 
+import (
+	"fmt"
+	"github/com/CargoMan0/GoPay/pkg/accountmanager"
+	"github/com/CargoMan0/GoPay/pkg/gen/auth"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
 /*
 func NewOperationFeedClient(addr string) (currency.CurrencyServiceClient, *grpc.ClientConn, error) {
 	dialOptions := []grpc.DialOption{
@@ -16,3 +24,33 @@ func NewOperationFeedClient(addr string) (currency.CurrencyServiceClient, *grpc.
 	return client, conn, nil
 }
 */
+
+func NewAuthClient(addr string) (auth.AuthServiceClient, *grpc.ClientConn, error) {
+	dialOptions := []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	}
+
+	conn, err := grpc.NewClient(addr, dialOptions...)
+	if err != nil {
+		return nil, nil, fmt.Errorf("grpc.NewClient: %w", err)
+	}
+
+	client := auth.NewAuthServiceClient(conn)
+
+	return client, conn, nil
+}
+
+func NewAccountManagerClient(addr string) (accountmanager.AccountManagerClient, *grpc.ClientConn, error) {
+	dialOptions := []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	}
+
+	conn, err := grpc.NewClient(addr, dialOptions...)
+	if err != nil {
+		return nil, nil, fmt.Errorf("grpc.NewClient: %w", err)
+	}
+
+	client := accountmanager.NewAccountManagerClient(conn)
+
+	return client, conn, nil
+}
